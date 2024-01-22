2651f85a8a728a0685994286456641e12ee876d8
Gabriel Ribeiro <gabriel.dev@h4money.com.br>
2024-01-22 08:06:55 -0300

    creating config struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
 use std::{ env, path::PathBuf };
 
-pub fn parse_arguments(args: &[String]) -> Result<(&str, PathBuf), String> {
-    if args.len() < 3 {
-        return Err(String::from("Too few arguments"));
+pub struct Config {
+    pub query: String,
+    pub file_path: PathBuf,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, String> {
+        if args.len() < 3 {
+            return Err(String::from("Too few arguments"));
+        }
+
+        let query = args[1].clone();
+        let file = &args[2];
+
+        let dir_name = env::current_dir().expect("Erro ao pegar o diretorio atual");
+
+        let file_path = dir_name.join(file);
+
+        Ok(Config { query, file_path })
     }
-
-    let query = &args[1];
-    let file = &args[2];
-
-    let dir_name = env::current_dir().expect("Erro ao pegar o diretorio atual");
-
-    let file_path = dir_name.join(file);
-
-    Ok((query, file_path))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use minigrep::parse_arguments;
 use std::env;
 use std::fs;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (_query, file_path) = parse_arguments(&args).expect("Erro");
+    let config = Config::new(&args).expect("erro");
 
-    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+    let contents = fs
+        ::read_to_string(config.file_path)
+        .expect("should have been able to read the file");
 
     println!("{}", contents);
 }
